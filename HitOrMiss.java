b8bbbf84d7c8f06a6b6ce6de5e6b70430beef2de
Rekha Chathoth <rekha.chathoth@gmail.com>
2019-10-16 22:53:11 -0400

    added the file

---
+++ HitOrMiss.java
@@ -0,0 +1,29 @@
+package programstatements;
+
+public class HitOrMiss {
+
+	public static void main(String[] args) {
+		/*boolean array[] = new boolean[10];*/
+		// TODO Auto-generated method stub
+		boolean array[]= {true,false,true,true,false,false,true,true,true,true};
+		for(int i=0;i<array.length;i++) {
+			if(array[i]) {
+		
+			System.out.println("Hit");
+		   }else
+		   {
+			   System.out.println("Miss");
+			} 
+		   }
+		/*else if (array[i]||array[i-1])
+		{
+			System.out.println("Miss");
+		}
+		else {
+			System.out.println("Miss and Hit");
+		}*/
+		//}
+	}
+
+}
+	
